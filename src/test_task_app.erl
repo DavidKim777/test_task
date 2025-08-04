f380151e94084aed3174c8bf01a2cc1c65df0b7b
Mac <mac@MacBook-Air-Mac.local>
2025-08-04 10:45:12 +0300

    branch-1

--- src/test_task_app.erl
+++ src/test_task_app.erl
@@ -6,8 +6,7 @@
 
 start(_Type, _Args) ->
 	Dispatch = cowboy_router:compile([
-		{'_', [{"/sport", test_task_sport_handler, []}]},
-		{'_', [{"/category", test_task_category_handler}]}
+		{'_', [{"/sport", test_task_sport_handler, []}]}
 	]),
 	{ok, _} = cowboy:start_clear(test_task_http_listener,
 		[{port, 8080}],
